0766a9c43e6b38f8872d2d7f184b56eece2f3bac
noesiux <noe.hernandez@gmail.com>
2024-09-09 11:50:52 -0600

    oks1

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Estate",  # The name that will appear in the App list
+    "name": "Estate NHDZ3",  # The name that will appear in the App list
     "version": "16.0.0",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
