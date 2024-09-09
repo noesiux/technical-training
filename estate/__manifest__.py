600ca37dddd39f1789e66c3cc9605335857fe897
noesiux <noe.hernandez@gmail.com>
2024-09-09 13:50:16 -0600

    t3

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Estate NHDZ3",  # The name that will appear in the App list
-    "version": "16.0.1",  # Version
+    "version": "16.1.1",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
