a75965b751a388e281a7af6a011128187de55b3d
noesiux <noe.hernandez@gmail.com>
2024-09-10 09:26:21 -0600

    d6

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,4 +4,18 @@ class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate Property'
 
-    name = fields.Char(string='Title', required=True)  # Único campo
+    name = fields.Char(string='Title')  # Campo 'name' de tipo Char
+    description = fields.Text(string='Description')  # Campo 'description' de tipo Text
+    postcode = fields.Char(string='Postcode')  # Campo 'postcode' de tipo Char
+    date_availability = fields.Date(string='Available From')  # Campo 'date_availability' de tipo Date
+    expected_price = fields.Float(string='Expected Price')  # Campo 'expected_price' de tipo Float (double precision)
+    selling_price = fields.Float(string='Selling Price')  # Campo 'selling_price' de tipo Float (double precision)
+    bedrooms = fields.Integer(string='Bedrooms')  # Campo 'bedrooms' de tipo Integer
+    living_area = fields.Integer(string='Living Area (sqm)')  # Campo 'living_area' de tipo Integer
+    facades = fields.Integer(string='Facades')  # Campo 'facades' de tipo Integer
+    garage = fields.Boolean(string='Garage')  # Campo 'garage' de tipo Boolean
+    garden = fields.Boolean(string='Garden')  # Campo 'garden' de tipo Boolean
+    garden_area = fields.Integer(string='Garden Area (sqm)')  # Campo 'garden_area' de tipo Integer
+    garden_orientation = fields.Char(string='Garden Orientation')  # Campo 'garden_orientation' de tipo Char
+
+    # Los campos 'create_uid', 'create_date', 'write_uid', 'write_date' se crean automáticamente por Odoo
